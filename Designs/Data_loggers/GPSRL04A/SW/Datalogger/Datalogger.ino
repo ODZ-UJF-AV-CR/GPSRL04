b53851c4c127588e47227691ea5809d1b4cdc865
kakl <kakl@f55caa52-0715-0410-8694-db4083128028>
2015-02-10 16:50:21 +0000

    Added comments to SW source.

--- Designs/Data_loggers/GPSRL04A/SW/Datalogger/Datalogger.ino
+++ Designs/Data_loggers/GPSRL04A/SW/Datalogger/Datalogger.ino
@@ -1,57 +1,41 @@
 /*
-  SD card datalogger
- 
- This example shows how to log data from three analog sensors 
- to an SD card using the SD library.
+  SD card datalogger GPSRL04A
  	
  The circuit:
- * analog sensors on analog ins 0, 1, and 2
+ * analog sensors are connected to analog ins PC0, PC1
  * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
+ ** MOSI - pin PB3
+ ** MISO - pin PB4
+ ** CLK  - pin PB5
+ ** CS   - pin PD4
  
- created  24 Nov 2010
- modified 9 Apr 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- 	 
- */
+*/
 
 #include <SD.h>
 
-
-// On the Ethernet Shield, CS is pin 4. Note that even if it's not
-// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
-// 53 on the Mega) must be left as an output or the SD library
-// functions will not work.
-const int chipSelect = 4;
-int led = 5;
-unsigned int n = 0;
-char inChar;
-String dataString = "";
-String filename = "log.csv";
-int coll = 0;
+const int chipSelect = 4;     // CS is PD4
+int LED = 5;                  // LED is conected from PD5 to LED at GPS module
+unsigned int n = 0;           // measurement counter
+char inChar;                  // input character from GPS
+String dataString = "";       // concantenated string with NMEA messages and measured values
+String filename = "log.csv";  // generated filename for log file
+int coll = 0;                 // collons counter in NMEA messages
 
 void setup()
 {
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+  while (!Serial) {;}
 
-
-  Serial.print("Initializing SD card...");
+  Serial.print("Initializing SD card...");  // inserting a SD Card always reset the processor and call setup
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
-  pinMode(10, OUTPUT);
-  pinMode(led, OUTPUT);     
+  pinMode(10, OUTPUT); // PB2
+  pinMode(LED, OUTPUT);     
 
   // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(chipSelect)) 
+  {
     Serial.println("Card failed, or not present");
     // don't do anything more:
     return;
@@ -59,50 +43,47 @@ void setup()
   Serial.println("card initialized.");
 
   dataString = "";
-  ReadGPRMC();
-
-//  filename = dataString.substring(0,5) + dataString.substring(dataString.length()-5,dataString.length()) + filename;
+  ReadGPRMC();     // read time from GPS
+  // derive file name fom time (YYMMDDHH.CSV)
   int len = dataString.length()-1;
   filename = dataString.substring(len-2,len) + dataString.substring(len-4,len-2) + dataString.substring(len-6,len-4) + dataString.substring(0,2) + ".CSV ";
 }
 
 void loop()
 { 
-  dataString = "";
-  ReadGPRMC();
+  dataString = "";        // make a string for assembling the data to log
+  ReadGPRMC();            // read NMEA sentences from GPS
   ReadGPGGA();
   
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(20);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(20);               // wait for a second
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(20);               // wait for a while
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(20);               // wait for a while
 
   // make a string for assembling the data to log:
-//  String dataString = "";
-  
   dataString += String(n++);
   dataString += ","; 
 
-  // read three sensors and append to the string:
+  // read analog values from sensors and append to the string:
   for (int analogPin = 0; analogPin < 2; analogPin++) 
   {
     int sensor = analogRead(analogPin);
     dataString += String(sensor);
-    if (analogPin < 1) 
+    if (analogPin < 1)   // suppress last coma
     {
       dataString += ","; 
     }
   }
 
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
   char fileNameCharArray[filename.length()];
   filename.toCharArray(fileNameCharArray, filename.length());
   File dataFile = SD.open(fileNameCharArray, FILE_WRITE);
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  //File dataFile = SD.open(filename, FILE_WRITE);
 
   // if the file is available, write to it:
-  if (dataFile) {
+  if (dataFile) 
+  {
     dataFile.println(dataString);
     dataFile.close();
     // print to the serial port too:
@@ -110,18 +91,19 @@ void loop()
   }  
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening datalog.txt");
+    Serial.println("error opening datalog.CSV");
   } 
 }
 
 
+// function for reading $GPRMC NMEA message
 void ReadGPRMC()
 {
   // $GPRMC,091451.00,A,4915.64143,N,01441.50397,E,0.053,,090215,,,A*74
 
   coll = 0;
   
-  while(1)
+  while(1)    // wait for $GPRMC
   {
     // get incoming byte:
     while (!Serial.available());
@@ -148,19 +130,19 @@ void ReadGPRMC()
     if (inChar == ',') coll++;
     dataString += inChar;
   }
-  while (coll < 9);
+  while (coll < 9);  // read only 9 coma separated values
 }
 
 
+// function for reading $GPGGA NMEA message
 void ReadGPGGA()
 {
   // $GPGGA,091451.00,4915.64143,N,01441.50397,E,1,09,0.90,443.2,M,44.0,M,,*50
 
   coll = 0;
   
-  while(1)
+  while(1)  // wait for $GPGGA
   {
-    // get incoming byte:
     while (!Serial.available());
     if (Serial.read() != '$') continue;
     while (!Serial.available());
@@ -183,9 +165,9 @@ void ReadGPGGA()
     while (!Serial.available());
     inChar = (char)Serial.read();
     if (inChar == ',') coll++;
-    if (coll > 4) dataString += inChar;
+    if (coll > 4) dataString += inChar;  // skip first 5 coma separated values
   }
-  while (coll < 12);
+  while (coll < 12); // read only 7 coma separated values
 }
 
 
